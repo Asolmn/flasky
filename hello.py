a8a23d31ece5f4a66e7b06cc5bf8f62f717b05a7
Asolmn <35882902+Asolmn@users.noreply.github.com>
2021-04-21 16:44:40 +0800

    feat:Add SQL Model

--- hello.py
+++ hello.py
@@ -1,29 +1,61 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_bootstrap import  Bootstrap
+from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FileField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='asolmn'
+app.config['SECRET_KEY'] = "asolmn"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    users = db.relationship('User',backref='role')
+
+    def __repr__(self):
+        return '<Role %r' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-    if form.validate_on_submit(): # 验证数据是否能被所有验证函数接受
+    if form.validate_on_submit():  # 验证数据是否能被所有验证函数接受
         old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data: # old_name不为None同时不等于上次表单中的数据
+        if old_name is not None and old_name != form.name.data:  # old_name不为None同时不等于上次表单中的数据
             flash('Looks like you have changed your name!')
-        session['name'] = form.name.data # 获取表单中name字段的内容
+        session['name'] = form.name.data  # 获取表单中name字段的内容
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
 
@@ -41,7 +73,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-
 
 
 if __name__ == '__main__':
